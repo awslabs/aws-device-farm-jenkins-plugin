6e2887a7c62bb9a083694da1ca01301bda53eab2
Rohan Deshpande <deshpan@amazon.com>
2017-05-21 12:22:58 -0700

    o Updated LICENSE. o Fix formatting.

--- src/main/java/org/jenkinsci/plugins/awsdevicefarm/AWSDeviceFarmUploadType.java
+++ src/main/java/org/jenkinsci/plugins/awsdevicefarm/AWSDeviceFarmUploadType.java
@@ -1,3 +1,17 @@
+//
+// Copyright 2010-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License").
+// You may not use this file except in compliance with the License.
+// A copy of the License is located at
+//
+// http://aws.amazon.com/apache2.0
+//
+// or in the "license" file accompanying this file. This file is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+//
 package org.jenkinsci.plugins.awsdevicefarm;
 
 import java.util.HashMap;
@@ -7,16 +21,16 @@ import java.util.Map;
  * This takes the place of the AWS Device Farm SDK type UploadType.
  */
 public enum AWSDeviceFarmUploadType {
-    ANDROID_APP ("ANDROID_APP"),                            // App
-    IOS_APP ("IOS_APP"),                                    // App
+    ANDROID_APP("ANDROID_APP"),                            // App
+    IOS_APP("IOS_APP"),                                    // App
     APPIUM_JAVA_TESTNG("APPIUM_JAVA_TESTNG_TEST_PACKAGE"),  // Test
     APPIUM_JAVA_JUNIT("APPIUM_JAVA_JUNIT_TEST_PACKAGE"),    // Test
     APPIUM_PYTHON("APPIUM_PYTHON_TEST_PACKAGE"),            // Test
-    CALABASH ("CALABASH_TEST_PACKAGE"),                     // Test
-    INSTRUMENTATION ("INSTRUMENTATION_TEST_PACKAGE"),       // Test
-    UIAUTOMATOR ("UIAUTOMATOR_TEST_PACKAGE"),               // Test
-    UIAUTOMATION ("UIAUTOMATION_TEST_PACKAGE"),             // Test
-    XCTEST ("XCTEST_TEST_PACKAGE"),                         // Test
+    CALABASH("CALABASH_TEST_PACKAGE"),                     // Test
+    INSTRUMENTATION("INSTRUMENTATION_TEST_PACKAGE"),       // Test
+    UIAUTOMATOR("UIAUTOMATOR_TEST_PACKAGE"),               // Test
+    UIAUTOMATION("UIAUTOMATION_TEST_PACKAGE"),             // Test
+    XCTEST("XCTEST_TEST_PACKAGE"),                         // Test
     XCTEST_UI("XCTEST_UI_TEST_PACKAGE");                    // Test
 
     private final String type;
@@ -25,6 +39,7 @@ public enum AWSDeviceFarmUploadType {
 
     /**
      * The constructor.
+     *
      * @param type The type.
      */
     private AWSDeviceFarmUploadType(final String type) {
@@ -41,6 +56,7 @@ public enum AWSDeviceFarmUploadType {
 
     /**
      * Getter for getting the enum by value.
+     *
      * @param s The String representation of the enum.
      * @return The corresponding enum value.
      */
@@ -50,10 +66,11 @@ public enum AWSDeviceFarmUploadType {
 
     /**
      * Convert the enum to a string.
+     *
      * @return The enum as a String.
      */
     @Override
-    public String toString(){
+    public String toString() {
         return type;
     }
 }
